f7128331dd73a9e86aad08089591b64ca1dba2ca
Kael Rodrigues Requena <87992854+kaelrequena@users.noreply.github.com>
2025-07-09 00:15:37 -0300

    Iniciando projeto

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    include_once("config/url.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Agenda de contatos</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </head>
+    <body>
+        <h1>testando agenda</h1>
+        <i class="fas fa-eye"></i>
+    </body>
+</html>
